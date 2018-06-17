790da28117cb985022220b997c9d049bca7681f0
Kybernetikus <kybernetikus@gmail.com>
2018-06-17 18:10:07 +0200

    Delete caption.js

--- js/caption.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  [].forEach.call(document.querySelectorAll('.content img[title]'), el => {
-    const caption = document.createElement('span')
-    caption.innerText = el.getAttribute('title')
-    caption.classList.add('caption')
-    el.parentNode.insertBefore(caption, el.nextSibling)
-  })
-})()
